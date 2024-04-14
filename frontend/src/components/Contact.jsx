2e67646952db18c4fe004d520660c179ed972fdf
saadhssn <128217038+saadhssn@users.noreply.github.com>
2024-04-14 23:48:14 +0500

    last commit

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://shy-blue-goose-hat.cyclic.app/api/v1/message/send",
         {
           name,
           email,
